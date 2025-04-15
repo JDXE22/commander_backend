d388ae6cb803ed72d86e20b915bb1c128f72896c
David <davidesparzaj22@gmail.com>
2025-04-15 08:46:48 -0500

    feat(commander/models/mongo): set up Command  model using Mongoose

--- src/models/mongo/commandModel.js
+++ src/models/mongo/commandModel.js
@@ -1,20 +1,15 @@
-
 import mongoose from "mongoose";
-import { MongoClient, ServerApiVersion, ObjectId } from "mongodb";
+import { commandSchema } from "../../schemas/mongo-schema/commandSchema.js";
 
-const uri = process.env.DATABASE_URL
-const client = new MongoClient(uri, {
-    serverApi: {
-        version: ServerApiVersion.v1,
-        strict: true,
-        deprecationErrors: true
-    }
-})
+const url = process.env.DATABASE_URL;
+const clientOptions = {
+  serverApi: { version: "1", strict: true, deprecationErrors: true },
+};
 
 async function connect() {
   try {
-    await mongoose.connect(uri, client);
-    await mongoose.connection.db.admin().command({ ping: 1})
+    await mongoose.connect(url, clientOptions);
+    await mongoose.connection.db.admin().command({ ping: 1 });
     console.log("Connected to MongoDB");
   } catch (error) {
     console.error("Error connecting to the database");
@@ -23,12 +18,16 @@ async function connect() {
   }
 }
 
-export class CommandModel {
-  // getAll = async () => {
-  //   const db = await connect();
+const commandMongooseModel = mongoose.model("Command", commandSchema, "commands");
 
-  //   return 
-  // };
+export class CommandModel {
+  getAll = async () => {
+    await connect();
+
+    const result = await commandMongooseModel.find({});
+
+    return result;
+  };
 
   // getById = async ({ id }) => {
   //   const db = await connect();
@@ -51,34 +50,33 @@ export class CommandModel {
   //   return db.find({}).toArray()
   // };
 
-//   createCommand = ({ input }) => {
-//     const newCommand = {
-//       id: commands.length + 1,
-//       ...input,
-//     };
+  //   createCommand = ({ input }) => {
+  //     const newCommand = {
+  //       id: commands.length + 1,
+  //       ...input,
+  //     };
 
-//     commands.push(newCommand);
+  //     commands.push(newCommand);
 
-//     return newCommand;
-//   };
+  //     return newCommand;
+  //   };
 
-//   updateCommand = ({ id, input }) => {
-//     const commandIndex = commands.findIndex((cmd) => cmd.id === id);
-//     if (commandIndex !== -1) {
-//       commands[commandIndex] = {
-//         ...commands[commandIndex],
-//         ...input,
-//       };
-//       return commands[commandIndex];
-//     }
-//   };
+  //   updateCommand = ({ id, input }) => {
+  //     const commandIndex = commands.findIndex((cmd) => cmd.id === id);
+  //     if (commandIndex !== -1) {
+  //       commands[commandIndex] = {
+  //         ...commands[commandIndex],
+  //         ...input,
+  //       };
+  //       return commands[commandIndex];
+  //     }
+  //   };
 
-//   delete = ({ id }) => {
-//     const commandById = commands.findIndex((cmd) => cmd.id === id);
-//     if (commandById === -1) return false;
-//     commands.splice(commandById, 1);
+  //   delete = ({ id }) => {
+  //     const commandById = commands.findIndex((cmd) => cmd.id === id);
+  //     if (commandById === -1) return false;
+  //     commands.splice(commandById, 1);
 
-//     return { message: `Command has been deleted successfully` };
-//   };
+  //     return { message: `Command has been deleted successfully` };
+  //   };
 }
- 
