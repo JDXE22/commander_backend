06d39ace7c30c018d612acaf3dffe808264f1a3f
David <davidesparzaj22@gmail.com>
2025-04-17 12:20:25 -0500

    feat(commander/src/uitls): add global error handler in utils to manage 500 errors

---
+++ utils/errors.js
@@ -0,0 +1,8 @@
+export const ErrorValidation = ((err, req, res, next) => {
+    const status = err.status || 500;
+    const message =
+      status === 500
+        ? 'Internal server error'
+        : err.message;
+    res.status(status).json({ message });
+  });
