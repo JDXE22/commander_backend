f901701fccbd03ce5be5e4e8820ac00c7f35eb9c
David <davidesparzaj22@gmail.com>
2025-04-23 21:46:53 -0500

    chore(commander/src/web): add styles folder and import new CSS stylesheet in index.html

--- src/web/index.js
+++ src/web/index.js
@@ -1,32 +1,48 @@
 const inputBox = document.getElementById("consoleInput");
 const responseArea = document.getElementById("responseArea");
+const copyButton = document.getElementById("copyButton");
 
-inputBox.addEventListener("keydown", textAreaListener );
+inputBox.addEventListener("keydown", textAreaListener);
+copyButton.addEventListener("click", copyToClipboard);
 
-async function textAreaListener (event) {
-    if (event.key === "Enter") {
-      const userCommand = event.target.value.trim();
-      if (!userCommand) return;
-  
-      responseArea.textContent = "Thinking...";
-  
-      try {
-        const res = await fetch(
-          `http://localhost:3000/command/cmd/${encodeURIComponent(
-            userCommand
-          )}`
-        );
-  
-        if (!res.ok) {
-          responseArea.textContent = "Command not found";
-          return;
-        }
-        const { text } = await res.json();
-  
-        responseArea.textContent = text;
-      } catch (error) {
-        responseArea.textContent = "Oops! Something went wrong";
-        console.error(error);
+async function textAreaListener(event) {
+  if (event.key === "Enter") {
+    const userCommand = event.target.value.trim();
+    if (!userCommand) return;
+
+    responseArea.textContent = "Thinking...";
+
+    try {
+      const res = await fetch(
+        `http://localhost:3000/command/cmd/${encodeURIComponent(userCommand)}`
+      );
+
+      if (!res.ok) {
+        responseArea.textContent = "Command not found";
+        return;
       }
+      const { text } = await res.json();
+
+      responseArea.textContent = text;
+    } catch (error) {
+      responseArea.textContent = "Oops! Something went wrong";
+      console.error(error);
     }
   }
+}
+
+async function copyToClipboard() {
+  navigator.clipboard
+    .writeText(responseArea.textContent)
+    .then(() => {
+      console.log("Text copied to clipboard");
+      copyButton.textContent = "Copied!";
+      setTimeout(() => {
+        copyButton.textContent = "Copy Text";
+      }, 2000);
+    })
+    .catch((err) => {
+      console.error("Error copying text", err);
+      copyButton.textContent = "Error!";
+    });
+}
