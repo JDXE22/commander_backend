aa1eb0469fb2d11d06e5c5520b487d82251cfbdc
David <davidesparzaj22@gmail.com>
2025-06-18 20:58:26 -0500

    refactor(backend/src/app.js):
    use CORS middleware from package
    
    and update import statements

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,33 +1,32 @@
 import express from "express";
 import "dotenv/config";
 import { commandRouter } from "./router/router.js";
-import { errorHandler}  from "./utils/errors.js";
-import { corsMiddleware } from "./utils/middlewares/cors.js";
-import path from 'path';
-import { fileURLToPath } from 'url';
-import { dirname}      from 'path';
+import { errorHandler } from "./utils/errors.js";
+import cors from "cors";
+import path from "path";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
 
 export const createApp = ({ commandModel }) => {
-  console.log('🌱 createApp running')
+  console.log("🌱 createApp running");
   const __filename = fileURLToPath(import.meta.url);
 
-  const __dirname  = dirname(__filename);
-  
+  const __dirname = dirname(__filename);
+
   const app = express();
 
-  app.use(express.static(path.join(__dirname, 'web')));
+  app.use(cors());
+
+  app.use(express.static(path.join(__dirname, "web")));
 
   const PORT = process.env.PORT || 1234;
 
   app.use(express.json());
   app.use(express.urlencoded({ extended: false }));
 
-  app.use(errorHandler);
-
-  app.use(corsMiddleware())
-
-
   app.use("/command", commandRouter({ commandModel }));
+
+  app.use(errorHandler);
 
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
